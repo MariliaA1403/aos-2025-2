8a4f29cca24f8412ff4ede43bf366929011cb275
MariliaA1403 <marilia.albuquerquelima@gmail.com>
2025-09-26 14:54:51 +0000

    .

--- atividade-tarefas/api/app.js
+++ atividade-tarefas/api/app.js
@@ -6,24 +6,24 @@ dotenv.config();
 
 const app = express();
 
-// Habilita o Express a receber JSON no corpo das requisições
+  
 app.use(express.json());
 
-// Rota principal
+
 app.get("/", (req, res) => {
   res.send("API de Tarefas funcionando! ✅");
 });
 
-// Rotas da API de tarefas
+
 app.use("/tarefas", tarefasRouter);
 
-// Porta do servidor
+
 const PORT = process.env.PORT || 3000;
 
-// Inicializa o servidor (apenas para ambiente local)
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
 
-// Exporta o app (útil para testes e Vercel no futuro)
+
 export default app;
